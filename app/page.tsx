d1f4e978efdc0023f2f4101950266947c6ccb62e
Nymora-World <nymoraworld@gmail.com>
2025-06-30 11:19:57 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center h-screen">
       <div>
-        <h1 className="text-center text-[5rem] antialiased leading-tight text-transparent bg-gradient-to-tl from-zinc-200 via-zinc-400 to-zinc-600 bg-clip-text">
+        <h1 className="text-center text-[3rem] md:text-[5rem] lg:text-[5rem] antialiased leading-tight text-transparent bg-gradient-to-tl from-zinc-200 via-zinc-400 to-zinc-600 bg-clip-text">
           XploitAgent.
         </h1>
-        <p className="text-center mt-2">
+        <p className="text-center text-xs lg:text-base md:text-base mt-2">
           This is a Vulnerability test site for XploitAgent
         </p>
       </div>
